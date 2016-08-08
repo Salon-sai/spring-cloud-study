39f8236c31c6fc79dfecc5603b267e64f32768b5
Salon.sai <coke1231078@gmail.com>
2016-08-08 17:19:42 +0800

    fix the config

---
+++ spring-cloud-server/src/main/java/org/salon/cloud/RefreshController.java
@@ -0,0 +1,12 @@
+package org.salon.cloud;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sai on 16-8-8.
+ */
+@RestController
+public class RefreshController
+{
+
+}
